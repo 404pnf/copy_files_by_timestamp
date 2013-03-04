3a1d80daefc05bc2b16ade1a37584dd6c422af3a
404 <404@example.com>
2013-03-04 16:52:53 +0800

    clean up ruby script

--- copy-by-timestamp.rb
+++ copy-by-timestamp.rb
@@ -4,12 +4,11 @@ require 'date'
 require 'set'
 require 'fileutils'
 
-
 # USAGE: ruby script.rb inputdir outpudir date
 
 puts "USAGE: ruby script.rb inputdir outputdir 20121202\n
       这条命令会将所有2012年12月2日后变更过的文件从inputdir复制到outputdir。会保留目录结构的。"  unless ARGV.size == 3
-                                                                                                     #p ARGV
+                                                                                                    
 src_dir, out_dir, date = ARGV
 
 # 检查一下日期输入是否正确，这个错了最不容易察觉
@@ -18,6 +17,7 @@ puts "日期格式必须是yyymmdd，比如20121015"  unless date =~ /[0-9]{8}/
 # 绝对目录
 abs_src_dir = File.expand_path src_dir
 abs_out_dir = File.expand_path out_dir
+
 # 如果输出的目录不存在就创建它吧
 FileUtils.mkdir abs_out_dir unless File.exists? abs_out_dir
 
@@ -33,7 +33,7 @@ time = date_str.to_time.to_i
 p "复制#{date_str}之后更新的文件到#{abs_out_dir}"
 
 # 找到需要复制的
-p files_to_copy = all_files.to_a.select {|f| f if File.file? f and File.mtime(f).to_i > time}
+files_to_copy = all_files.to_a.select {|f| f if File.file? f and File.mtime(f).to_i > time}
 
 # 目标文件的名字
 dest_files = files_to_copy.map {|f| File.dirname(f).sub(abs_src_dir, abs_out_dir)}
